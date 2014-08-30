4c588275b31f09f2269ba2139d10770df3ce5d76
Dregath <lachlan@dregath.com>
2014-08-30 22:55:10 +1000

    Created Main Class

---
+++ src/main/java/com/dregath/permaculture/Permaculture.java
@@ -0,0 +1,30 @@
+package com.dregath.permaculture;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Lachlan on 30/08/2014.
+ */
+@Mod(modid = "permaculture", name = "Permaculture", version = "1.7.10-0.1")
+public class Permaculture {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
